314906e9c960e717576dd9e96100e3e5be99039a
standardforever <standard.forever123@gmail.com>
2022-06-22 15:59:34 -0400

    updated

--- script.js
+++ script.js
@@ -1,46 +1,8 @@
-let num1 = prompt('Enter Your First Number:');
+let Name = "Yusuf Kusimo";
+let height = "6.5ft";
+let country = "Nigeria";
 
-let num2= prompt('Enter Your second Number:');
-let _sign = prompt("Enter The Operator '*, +, -, /'");
 
-function _addition(num1, num2)
-{   
-	const sum = num1 + num2
-	alert("Your anwer is:  " + sum);
-}
-
-function _subtract(num1, num2)
-{
-	alert("Your anwer is:  " + (num1 - num2));
-}
-
-function _divide(num1, num2)
-{
-	alert("Your anwer is:  " + num1 / num2);
-}
-
-function _multiply(num1, mum2)
-{
-	alert("Your anwer is:  " + num1 * num2)
-}
-
-if (_sign == '*')
-{
-	_multiply(num1, num2);
-}
-else if(_sign == '/')
-{
-	_divide(num1, num2);
-}
-else if(_sign == '+')
-{
-	_addition(num1, num2);
-}
-else if (_sign == '-')
-{
-	_subtract(num1, num2);
-}
-
-else{
-	alert("Input is Wrong");
-}
+alert("My Name is: " +  Name);
+alert("My Height is: " + height);
+alert("My Country is: " + country);
