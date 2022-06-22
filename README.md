37ad4d2e392c90ad44d8dded902aef3179b9ec3b
standardforever <standard.forever123@gmail.com>
2022-06-22 07:33:04 -0400

    updated

---
+++ README.md
@@ -0,0 +1,1 @@
+Write a javascript code that prints out your name, height, and country on the screen
---
+++ script.js
@@ -0,0 +1,8 @@
+const Name = "Yusuf Kusimo";
+const height = "6.5ft";
+const country = "Nigeria";
+
+
+alert("My Name is: " +  Name);
+alert("My Height is: " + height);
+alert("My Country is: " + country);
